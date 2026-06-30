1223cbf0f0446721ea2e65b823e9bf834d174186
setu <shibu.richi.masco@gmail.com>
2026-06-30 17:44:38 +0530

    Made scan display functional for pacman cache

--- utils/scan.py
+++ utils/scan.py
@@ -6,13 +6,18 @@ import os
 def scan_pacman():
     """Scans pacman cache"""
     results = {}
+    raw = {}
     path = "/var/cache/pacman/pkg"
     with os.scandir(path) as entries:
         for entry in entries:
             if entry.is_file():
                 base_name = entry.name.split(".pkg.tar")[0]
                 package_name = base_name.rsplit("-", 3)[0]
-                size = entry.stat().st_size
+                try:
+                    size = raw[package_name] + entry.stat().st_size
+                except KeyError:
+                    size = entry.stat().st_size
+                raw[package_name] = size
                 if size >= 1024**2:
                     results[package_name] = f"{size // 1024**2} MB"
                 elif size >= 1024:
